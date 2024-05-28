d1c7fdd06c2634ebd58033674986a53de00af1be
Marga <margamatunog@gmail.com>
2024-05-28 20:45:20 +0800

    asdasd

--- app/src/main/java/com/example/foodguard/MainActivity.java
+++ app/src/main/java/com/example/foodguard/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.content.Intent;
 import android.os.Bundle;
